8108d93fda31a6005b6bdcdb13ec48403a6d6be3
Peachy-Njenga <peacheswagithi16@gmail.com>
2024-06-12 15:36:58 +0300

    Fixed bug

---
+++ breakout_5/src/Brick.lua
@@ -0,0 +1,33 @@
+Brick = Class {}
+
+function Brick:init(x, y)
+    self.tier = 0
+    self.color = 1
+
+    self.x = x
+    self.y = y
+    self.width = 32
+    self.height = 16
+
+    --should brick be rendered
+    self.inPlay = true
+end
+
+--[[
+    Triggers when brck is hit
+]]
+
+function Brick:hit()
+    --sounds
+    gSounds['brick-hit-2']:play()
+
+    self.inPlay = false
+end
+
+function Brick:render()
+    if self.inPlay then
+        love.graphics.draw(gTextures['main'],
+            gFrames['bricks'][1 + ((self.color - 1) * 4) + self.tier],
+            self.x, self.y)
+    end
+end
